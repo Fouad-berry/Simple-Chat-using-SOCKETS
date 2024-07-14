54631812a0166973c577232e9a691b25403f3709
Fouad-berry <fidriss96@gmail.com>
2024-07-14 15:56:05 +0200

    update project

--- chat-frontend/src/App.jsx
+++ chat-frontend/src/App.jsx
@@ -116,7 +116,7 @@ function App() {
       <div className="mainChat">
         <div className="flex">
           <div className="userList">
-            <h3>Les Users : {userCount}</h3>
+            <h3>The Users : {userCount}</h3>
             <ul>
               <li onClick={() => {
                 setActiveChat('All');
@@ -177,7 +177,7 @@ function App() {
                 onChange={handleMessageChange}
               />
               <div className="vDivider"></div>
-              <button type="submit" className='sendButton'>Sent message <FontAwesomeIcon icon={faPaperPlane} /></button>
+              <button type="submit" className='sendButton'>Sent the message <FontAwesomeIcon icon={faPaperPlane} /></button>
             </form>
           </div>
         </div>
